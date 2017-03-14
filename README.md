4c2068c8166d47c4ef1c6df39a8499a76ddb6d4f
smarcu <smarcu@gmail.com>
2017-03-14 11:34:33 +0200

    STEP-2 subscriber

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Spring Reactor Demo 1
 
 example based on https://spring.io/blog/2016/06/13/notes-on-reactive-programming-part-ii-writing-some-code
+
+
+## Git tags for presentation:
+* STEP-1
+    * Flux, log, map, subscribe
+* STEP-2
+    * Subscriber
+    
--- src/main/java/com/example/SpringReactorDemo1Application.java
+++ src/main/java/com/example/SpringReactorDemo1Application.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
@@ -12,11 +14,29 @@ public class SpringReactorDemo1Application {
 		Flux<String> flux = Flux.just("red", "white", "blue");
 
 		Flux<String> upper = flux
-				.log()
 				.map(String::toUpperCase);
 
-		upper.subscribe(System.out::println);
+		upper.subscribe(new Subscriber<String>() {
+			@Override
+			public void onSubscribe(Subscription subscription) {
+				subscription.request(Long.MAX_VALUE);
+			}
 
+			@Override
+			public void onNext(String t) {
+				System.out.println("onNext: " + t);
+			}
+
+			@Override
+			public void onError(Throwable throwable) {
+				throwable.printStackTrace();
+			}
+
+			@Override
+			public void onComplete() {
+				System.out.println("onComplete");
+			}
+		});
 
 	}
 }
